7152c2823a1bf3a282d3b730a11070725350c6b8
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-01-24 16:27:20 -0500

    Adding more files

--- scripts/arrow.js
+++ scripts/arrow.js
@@ -1,9 +1,17 @@
-const person = {
- firstName: 'Grace',
- lastName: 'Hopper',
- age: 89,
- fullName: () => { return this.firstName + ' ' + this.lastName }
+firstName = "sudh"
+lastName = "last"
+{
+    firstName = "wow"
+    lastName = "tikka"
+    var person = {
+        firstName: 'Grace',
+        lastName: 'Hopper',
+        age: 89,
+        fullName: () => { return firstName + ' ' + lastName }
+       }
 }
+
 
 let x = person.fullName();
 console.log(x);
+
