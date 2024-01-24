3aea2f74c3711f869d0e186d751c6dd95deddf58
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-01-24 15:35:41 -0500

    Adding javascript looger quiz

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,15 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+        console.log(this.errMsg);
+}
+
+const object1 = {
+    errMsg:"Something went wrong!"
+}
+
+const object2 = {
+    errMsg:"Something went wrong! -- 2"
+}
+
+logMsg.call(object1)
+logMsg.call(object2)
