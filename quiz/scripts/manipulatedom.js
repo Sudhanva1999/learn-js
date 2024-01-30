0aa0cc3347f5199ea7d19a3f8440faf5f3f8f2c9
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-01-29 21:32:43 -0500

    Adding Javascript quizes

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -10,9 +10,10 @@ function createTRNode(colNodes) {
   return trNode;
 }
 
-function createTDNode(childNode) {
+function createTDNode(childNode, id) {
   let tdNode = document.createElement("td");
   tdNode.appendChild(childNode);
+  tdNode.setAttribute('id', id);
   return tdNode;
 }
 
@@ -21,11 +22,33 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createEditButton(id) {
+  let buttonElement = document.createElement("button");
+  buttonElement.appendChild(document.createTextNode('Edit Text'));
+  buttonElement.setAttribute('id', id);
+  return buttonElement;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"),i+"_td");
+    let col2 = createTDNode(createEditButton(i+'_button'));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
+  // Add event listeners
+  for(let i = 0; i < 3; i++) {
+    let tdNode = document.getElementById(i+"_td");
+    console.log(tdNode);
+    document.getElementById(i+'_button').addEventListener('click', function(e) {
+  
+    let inputTxt = document.createElement("input");
+    inputTxt.type = 'text';
+    inputTxt.placeholder = 'Enter cell(x,y)...';
+    tdNode.replaceChild(inputTxt, tdNode.childNodes[0]);
+    
+    });
+  }
+  
 }
